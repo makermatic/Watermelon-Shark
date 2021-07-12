313fecf7a7876bf252740b1465e8f69b72d02ad6
makermatic <44180512+makermatic@users.noreply.github.com>
2021-07-12 19:04:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new discord.Client
 const token = 'ODU3Nzk0MDc4ODI0MjAyMjQw.YNUw_Q.8okgBonGaQHkFqFsWqgkw6PZg_k'
 const prefix = '!'
 
-const words = ['*sh*a*r**k','S*H*A*R*K',':shark:', 'Watermelon Shark', 'watermelon shark', 'shark', 'Shark', 'SHARK', 'watermelon', 'Watermelon', 'WATER', 'water', 'MELON', 'melon', 'SHARK', 'FOOD', 'food', 'Food', 'FISH', 'fish', 'Fish']
+const words = ['*sh*a*r**k', 'S*H*A*R*K', ':shark:', 'Watermelon Shark', 'watermelon shark', 'shark', 'Shark', 'SHARK', 'watermelon', 'Watermelon', 'WATER', 'water', 'MELON', 'melon', 'SHARK', 'FOOD', 'food', 'Food', 'FISH', 'fish', 'Fish']
 
 bot.on('ready', () => {
     console.log('Bot is online you sick fuck.')
@@ -14,7 +14,7 @@ bot.on('message', msg => {
     //Word detector
     if (words.some(word => msg.content.includes(word))) {
         const image = new discord.MessageAttachment('./shark.png')
-        msg.reply (image)
+        msg.reply(image)
     }
 })
 
